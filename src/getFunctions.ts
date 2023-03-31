b97cca1fdb1cf9453e3c449bba5f537009502d55
ilcato <ilcato@yahoo.com>
2023-03-31 10:57:57 +0200

    fix values

--- src/getFunctions.ts
+++ src/getFunctions.ts
@@ -364,6 +364,8 @@ export class GetFunctions {
   }
 
   getCurrentDoorState(characteristic, service, IDs, properties) {
+    const v = parseInt(properties.value);
+    this.platform.log('getCurrentDoorState value:', v);
     switch (properties.state) {
       case 'Opened':
         characteristic.updateValue(this.platform.Characteristic.CurrentDoorState.OPEN);
@@ -377,13 +379,11 @@ export class GetFunctions {
       case 'Closed':
         characteristic.updateValue(this.platform.Characteristic.CurrentDoorState.CLOSED);
         break;
-      case 'Unknow':
-        characteristic.updateValue(this.platform.Characteristic.CurrentDoorState.STOPPED);
-        break;
+      case 'Unknown':
       case undefined:
-        if (properties.value === '99') {
+        if (v === 0) {
           characteristic.updateValue(this.platform.Characteristic.CurrentDoorState.CLOSED);
-        } else if (properties.value === '0') {
+        } else if (v === 99) {
           characteristic.updateValue(this.platform.Characteristic.CurrentDoorState.OPEN);
         } else {
           characteristic.updateValue(this.platform.Characteristic.CurrentDoorState.STOPPED);
@@ -396,6 +396,8 @@ export class GetFunctions {
   }
 
   getTargetDoorState(characteristic, service, IDs, properties) {
+    const v = parseInt(properties.value);
+    this.platform.log('getTargetDoorState value:', v);
     switch (properties.state) {
       case 'Opened':
         characteristic.updateValue(this.platform.Characteristic.CurrentDoorState.OPEN);
@@ -409,20 +411,18 @@ export class GetFunctions {
       case 'Closed':
         characteristic.updateValue(this.platform.Characteristic.CurrentDoorState.CLOSED);
         break;
-      case 'Unknow':
-        characteristic.updateValue(this.platform.Characteristic.CurrentDoorState.CLOSED);
-        break;
+      case 'Unknown':
       case undefined:
-        if (properties.value === '99') {
+        if (v === 0) {
           characteristic.updateValue(this.platform.Characteristic.CurrentDoorState.CLOSED);
-        } else if (properties.value === '0') {
+        } else if (v === 99) {
           characteristic.updateValue(this.platform.Characteristic.CurrentDoorState.OPEN);
         } else {
-          characteristic.updateValue(this.platform.Characteristic.CurrentDoorState.STOPPED);
+          characteristic.updateValue(this.platform.Characteristic.CurrentDoorState.CLOSED);
         }
         break;
       default:
-        characteristic.updateValue(this.platform.Characteristic.CurrentDoorState.STOPPED);
+        characteristic.updateValue(this.platform.Characteristic.CurrentDoorState.CLOSED);
         break;
     }
   }
