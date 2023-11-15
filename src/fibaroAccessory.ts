a3629fd7016b4d633063401870fc82fd683b1825
mkz212 <82271669+mkz212@users.noreply.github.com>
2023-11-15 09:04:55 +0100

    Update fibaroAccessory.ts

--- src/fibaroAccessory.ts
+++ src/fibaroAccessory.ts
@@ -181,7 +181,6 @@ export class FibaroAccessory {
       case 'com.fibaro.rollerShutter':
       case 'com.fibaro.FGWR111':
       case 'com.fibaro.remoteBaseShutter':
-      case 'com.fibaro.barrier':
       case 'com.fibaro.baseShutter': // only if favoritePositionsNativeSupport is true otherwise it's a garage door
         // it's a garage door
         // case 57 - gate with positioning
