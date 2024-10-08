f694b16d76861b0198b12ff5d41f5913baf331ec
ilcato <ilcato@yahoo.com>
2024-10-08 09:38:03 +0200

    removed not necessary comment

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,6 @@ import { API } from 'homebridge';
 import { PLATFORM_NAME } from './constants';
 import { FibaroHC } from './platform';
 
-/**
- * This method registers the platform with Homebridge
- */
 export = (api: API) => {
   api.registerPlatform(PLATFORM_NAME, FibaroHC);
 };
