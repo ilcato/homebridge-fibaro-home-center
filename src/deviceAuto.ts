810511a546bf7bdc9d11210b0620e273198edb13
ilcato <ilcato@yahoo.com>
2024-10-19 13:01:03 +0200

    added 'com.fibaro.thermostatDanfoss'

--- src/deviceAuto.ts
+++ src/deviceAuto.ts
@@ -406,6 +406,22 @@ export class DeviceConfigurations {
     return;
   }
 
+  // Radiator thermostatic valve.
+  @DeviceType('com.fibaro.thermostatDanfoss')
+  private static radiatorThermostaticValve(Service, Characteristic, device) {
+    return [{
+      service: Service.Thermostat,
+      characteristics: [
+        Characteristic.CurrentTemperature,
+        Characteristic.TargetTemperature,
+        Characteristic.CurrentHeatingCoolingState,
+        Characteristic.TargetHeatingCoolingState,
+        Characteristic.TemperatureDisplayUnits,
+      ],
+      subtype: device.id + '--' + constants.SUBTYPE_RADIATOR_THERMOSTATIC_VALVE,
+    }];
+  }
+
   // Security system
   @DeviceType(constants.DEVICE_TYPE_SECURITY_SYSTEM)
   private static securitySystem(Service, Characteristic) {
